69991e136138f9d02b631d1966cfb2d88d0742ce
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-02-21 14:10:51 +0530

    Add files via upload

---
+++ 68_landmarks_detection/facial_landmarks.py
@@ -0,0 +1,47 @@
+# import the necessary packages
+from imutils import face_utils
+import dlib
+import cv2
+import os
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(os.path.join(os.getcwd(), r"shape_predictor_68_face_landmarks.dat"))
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = vid.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rects = detector(gray, 1)
+	# detect faces in the grayscale image
+
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		# convert dlib's rectangle to a OpenCV-style bounding box
+		# [i.e., (x, y, w, h)], then draw the face bounding box
+		(x, y, w, h) = face_utils.rect_to_bb(rect)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		# show the face number
+		cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		# loop over the (x, y)-coordinates for the facial landmarks
+		# and draw them on the image
+		for (x, y) in shape:
+			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+	cv2.imshow('Video', frame)
+
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
